1210a9317a44737bc3197709a2d3305a77c1e9f4
MelanieMartinMarcel <melanieluzmartinmarcel@gmail.com>
2024-06-17 20:30:46 -0300

    ACTUALIZO

--- RentACCar/reportes_estadisticas.h
+++ RentACCar/reportes_estadisticas.h
@@ -9,6 +9,18 @@ float calcularIngresosEnMes(Alquiler* alquileres, int cantidadAlquileres, int me
 // Función para calcular el alquiler que generó mayor ingreso
 Alquiler calcularAlquilerMayorIngreso(Alquiler* alquileres, int cantidadAlquileres);
 
+// Función para ver los vehículos disponibles con menos de 5 años, ordenados por antigüedad
+void verVehiculosDisponiblesRecientes(Vehiculo* vehiculos, int cantidadVehiculos);
+
+// Función para ver listado de alquileres por cliente
+void verAlquileresPorCliente(Alquiler* alquileres, int cantidadAlquileres, int dniCliente);
+
+
+void setColor(int textColor, int bgColor);
+void gotoxy(int x, int y);
+void dibujarCuadro(int x1, int y1, int x2, int y2);
+
+
 
 
 #endif // REPORTES_ESTADISTICAS_H_INCLUDED
